2a8272b2f914de121d71379066d5e9116dfe32f6
RikoSandria <Rifki.friend@gmail.com>
2018-07-05 18:45:57 +0700

    Add files via upload

---
+++ Part.java
@@ -0,0 +1,183 @@
+package com.project;
+
+import javax.media.opengl.GL;
+import javax.media.opengl.glu.GLU;
+import javax.media.opengl.glu.GLUquadric;
+
+/**
+ *
+ * @author Alfonsius Lorensius
+ */
+public class Part {
+
+    static GLU glu = new GLU();
+
+    static void pipe(GL gl, float diameter, float height) {
+        float radius = diameter / 2;
+        GLUquadric q = glu.gluNewQuadric();
+        gl.glPushMatrix();
+//        gl.glColor3f(0, 1, 1);
+        gl.glTranslatef(0.0f, height / 2, 0.0f);
+        gl.glRotatef(90.0f, 1.0f, 0.0f, 0.0f);
+        glu.gluCylinder(q, radius, radius, height, 36, 36);
+        gl.glPopMatrix();
+        glu.gluDeleteQuadric(q);
+    }
+
+    static void cylinder(GL gl, float diameter, float height) {
+        glu = new GLU();
+        float radius = diameter / 2;
+        GLUquadric q = glu.gluNewQuadric();
+        gl.glPushMatrix();
+//        gl.glColor3f(0, 1, 1);
+        gl.glTranslatef(0.0f, height / 2, 0.0f);
+        gl.glRotatef(90.0f, 1.0f, 0.0f, 0.0f);
+        glu.gluCylinder(q, radius, radius, height, 36, 36);
+//        gl.glColor3f(0.0f, 0.0f, 1.0f);
+        glu.gluDisk(q, 0, radius, 36, 36);
+        gl.glTranslatef(0.0f, 0.0f, height);
+        glu.gluDisk(q, 0, radius, 36, 36);
+        gl.glPopMatrix();
+        glu.gluDeleteQuadric(q);
+    }
+
+    static void cube(GL gl, float diameter, float height) {
+        glu = new GLU();
+        float radius = diameter / 2;
+        GLUquadric q = glu.gluNewQuadric();
+        gl.glPushMatrix();
+//        gl.glColor3f(0, 1, 1);
+        gl.glTranslatef(0.0f, height / 2, 0.0f);
+        gl.glRotatef(90.0f, 1.0f, 0.0f, 0.0f);
+        glu.gluCylinder(q, radius, radius, height, 4, 4);
+//        gl.glColor3f(0.0f, 0.0f, 1.0f);
+        glu.gluDisk(q, 0, radius, 4, 4);
+        gl.glTranslatef(0.0f, 0.0f, height);
+        glu.gluDisk(q, 0, radius, 4, 4);
+        gl.glPopMatrix();
+        glu.gluDeleteQuadric(q);
+    }
+
+    static void disk(GL gl, float inner, float outer) {
+        glu = new GLU();
+        GLUquadric q = glu.gluNewQuadric();
+        gl.glPushMatrix();
+//        gl.glColor3f(0, 1, 1);
+        gl.glRotatef(90.0f, 1.0f, 0.0f, 0.0f);
+        glu.gluDisk(q, inner / 2, outer / 2, 36, 36);
+        gl.glPopMatrix();
+        glu.gluDeleteQuadric(q);
+    }
+
+    static void pistonHead(GL gl) {
+        gl.glPushMatrix();
+        gl.glColor3f(0.85f, 0.85f, 0.85f);
+        pipe(gl, 4.0f, 3.0f);
+
+        gl.glPushMatrix();
+        gl.glTranslatef(0.0f, 1.5f, 0.0f);
+        gl.glColor3f(0.8f, 0.8f, 0.8f);
+        disk(gl, 0.0f, 4.0f);
+        gl.glTranslatef(0.0f, -3.0f, 0.0f);
+        disk(gl, 3.5f, 4.0f);
+        gl.glTranslatef(0.0f, 2.0f, 0.0f);
+        disk(gl, 0.0f, 3.5f);
+        gl.glTranslatef(0.0f, 0.3f, 0.0f);
+        cylinder(gl, 4.01f, 0.2f);
+        gl.glTranslatef(0.0f, 0.3f, 0.0f);
+        cylinder(gl, 4.01f, 0.2f);
+        gl.glPopMatrix();
+
+        gl.glTranslatef(0.0f, -0.5f, 0.0f);
+        gl.glColor3f(0.75f, 0.75f, 0.75f);
+        pipe(gl, 3.5f, 2.0f);
+
+        gl.glColor3f(0.65f, 0.65f, 0.65f);
+        gl.glRotatef(90.0f, 0.0f, 1.0f, 0.0f);
+        gl.glRotatef(90.0f, 0.0f, 0.0f, 1.0f);
+        cylinder(gl, 1, 3.5f);
+        gl.glPopMatrix();
+    }
+
+    static void pistonStem(GL gl) {
+        GLUquadric q = glu.gluNewQuadric();
+        double[] eqn = {0.75, 0.0, 1.0, 0.0};
+
+        gl.glPushMatrix();
+        gl.glClipPlane(GL.GL_CLIP_PLANE0, eqn, 1);
+        gl.glEnable(GL.GL_CLIP_PLANE0);
+
+        gl.glPushMatrix();
+        gl.glRotatef(90.0f, 1.0f, 0.0f, 0.0f);
+        gl.glColor3f(0.85f, 0.85f, 0.85f);
+        pipe(gl, 4.0f, 1.5f);
+        pipe(gl, 2.5f, 1.5f);
+        gl.glColor3f(0.8f, 0.8f, 0.8f);
+        gl.glTranslatef(0.0f, 0.75f, 0.0f);
+        disk(gl, 2.5f, 4.0f);
+        gl.glTranslatef(0.0f, -1.5f, 0.0f);
+        disk(gl, 2.5f, 4.0f);
+        gl.glPopMatrix();
+        gl.glDisable(GL.GL_CLIP_PLANE0);
+
+//        gl.glTranslatef(0.0f, 0.70f, 0.0f);
+        gl.glPushMatrix();
+        gl.glTranslatef(1.7f, 0.75f, 0.0f);
+        feetCube(gl, 1.0f, 1.5f, 1.5f);
+        gl.glTranslatef(-3.4f, 0.0f, 0.0f);
+        feetCube(gl, 1.0f, 1.5f, 1.5f);
+        gl.glPopMatrix();
+        
+        gl.glPushMatrix();
+        gl.glTranslatef(0.0f, 5.0f, 0.0f);
+        feetCube(gl, 1.5f, 7.0f, 1.5f);
+        gl.glPopMatrix();
+        gl.glPopMatrix();
+    }
+
+    static void feetCube(GL gl, float rx, float ry, float rz) {
+        float x = rx / 2;
+        float y = ry / 2;
+        float z = rz / 2;
+        gl.glPushMatrix();
+        gl.glColor3f(0.85f, 0.85f, 0.85f);
+
+        gl.glBegin(GL.GL_POLYGON);
+        gl.glVertex3f(-x, y, -z);
+        gl.glVertex3f(x, y, -z);
+        gl.glVertex3f(x, y, z);
+        gl.glVertex3f(-x, y, z);
+        gl.glVertex3f(-x, -y, z);
+        gl.glVertex3f(-x, -y, -z);
+        gl.glEnd();
+
+        gl.glPushMatrix();
+        gl.glRotatef(180.0f, 0.0f, 0.0f, 1.0f);
+        gl.glBegin(GL.GL_POLYGON);
+        gl.glVertex3f(-x, y, -z);
+        gl.glVertex3f(x, y, -z);
+        gl.glVertex3f(x, y, z);
+        gl.glVertex3f(-x, y, z);
+        gl.glVertex3f(-x, -y, z);
+        gl.glVertex3f(-x, -y, -z);
+        gl.glEnd();
+        gl.glPopMatrix();
+
+        gl.glColor3f(0.8f, 0.8f, 0.8f);
+        gl.glBegin(GL.GL_QUADS);
+        gl.glVertex3f(-x, y, -z);
+        gl.glVertex3f(x, y, -z);
+        gl.glVertex3f(x, -y, -z);
+        gl.glVertex3f(-x, -y, -z);
+        gl.glEnd();
+        gl.glTranslatef(0.0f, 0.0f, rz);
+        gl.glBegin(GL.GL_QUADS);
+        gl.glVertex3f(-x, y, -z);
+        gl.glVertex3f(x, y, -z);
+        gl.glVertex3f(x, -y, -z);
+        gl.glVertex3f(-x, -y, -z);
+        gl.glEnd();
+        gl.glPopMatrix();
+    }
+
+}
---
+++ Piston.java
@@ -0,0 +1,121 @@
+package com.project;
+
+import com.sun.opengl.util.Animator;
+import java.awt.Frame;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import javax.media.opengl.GL;
+import javax.media.opengl.GLAutoDrawable;
+import javax.media.opengl.GLCanvas;
+import javax.media.opengl.GLEventListener;
+import javax.media.opengl.glu.GLU;
+
+/**
+ * Piston.java <BR>
+ * author: Brian Paul (converted to Java by Ron Cemer and Sven Goethel)
+ * <P>
+ *
+ * This version is equal to Brian Paul's version 1.2 1999/10/21
+ */
+public class Piston implements GLEventListener {
+
+    public static void main(String[] args) {
+        Frame frame = new Frame("Simple JOGL Application");
+        GLCanvas canvas = new GLCanvas();
+
+        canvas.addGLEventListener(new Piston());
+        frame.add(canvas);
+        frame.setSize(720, 540);
+        final Animator animator = new Animator(canvas);
+        frame.addWindowListener(new WindowAdapter() {
+
+            @Override
+            public void windowClosing(WindowEvent e) {
+                // Run this on another thread than the AWT event queue to
+                // make sure the call to Animator.stop() completes before
+                // exiting
+                new Thread(new Runnable() {
+
+                    public void run() {
+                        animator.stop();
+                        System.exit(0);
+                    }
+                }).start();
+            }
+        });
+        // Center frame
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+        animator.start();
+    }
+
+    public void init(GLAutoDrawable drawable) {
+        // Use debug pipeline
+        // drawable.setGL(new DebugGL(drawable.getGL()));
+
+        GL gl = drawable.getGL();
+        System.err.println("INIT GL IS: " + gl.getClass().getName());
+
+        // Enable VSync
+        gl.setSwapInterval(1);
+        
+        // supaya depth dirender dengan benar
+        gl.glEnable(GL.GL_DEPTH_TEST);
+
+        // Setup the drawing area and shading mode
+        gl.glClearColor(1.0f, 1.0f, 1.0f, 0.0f);
+        gl.glShadeModel(GL.GL_SMOOTH); // try setting this to GL_FLAT and see what happens.
+    }
+
+    public void reshape(GLAutoDrawable drawable, int x, int y, int width, int height) {
+        GL gl = drawable.getGL();
+        GLU glu = new GLU();
+
+        if (height <= 0) { // avoid a divide by zero error!
+
+            height = 1;
+        }
+        final float h = (float) width / (float) height;
+        gl.glViewport(0, 0, width, height);
+        gl.glMatrixMode(GL.GL_PROJECTION);
+        gl.glLoadIdentity();
+        glu.gluPerspective(50.0f, h, 1.0, 20.0);
+        gl.glMatrixMode(GL.GL_MODELVIEW);
+        gl.glLoadIdentity();
+    }
+    float angle = 0;
+
+    public void display(GLAutoDrawable drawable) {
+        GL gl = drawable.getGL();
+
+        // Clear the drawing area
+        gl.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT);
+        // Reset the current matrix to the "identity"
+        gl.glLoadIdentity();
+
+        // Move the "drawing cursor" around
+        // atur kamera
+        GLU glu = new GLU();
+        glu.gluLookAt(
+                0, 0, 15, // koordinat posisi kamera
+                0, 0, 0, // koordinat arah kamera (kamera menghadap ke mana)
+                0, 1, 0 // koordinat arah atas kamera
+        );
+        
+        gl.glRotatef(angle++, 1.0f, 0.0f, 0.0f);
+        // Drawing Using Triangles
+
+        gl.glTranslatef(0.0f, 3.0f, 0.0f);
+        Part.pistonHead(gl);
+//        Part.pistonStem(gl);
+        gl.glTranslatef(0.0f, -8.0f, 0.0f);
+        Part.pistonStem(gl);
+        // Done Drawing The Quad
+
+        // Flush all drawing operations to the graphics card
+        gl.glFlush();
+    }
+
+    public void displayChanged(GLAutoDrawable drawable, boolean modeChanged, boolean deviceChanged) {
+    }
+}
